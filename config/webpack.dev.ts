eb2f0ccd33e84cab35f0a95689a5c34a153ac588
Gavin.guo <Gavin.guo@dfox.cn>
2021-01-03 03:04:51 +0800

    解决dev-server无法刷新的问题

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'path';
 import { merge } from 'webpack-merge';
-
+import { HotModuleReplacementPlugin } from 'webpack';
 import commonConfig from "./webpack.common";
 
 const devConfig = merge(commonConfig, {
+    target: "web",
     mode: "development",
-    devtool: 'eval-source-map',
+    plugins: [new HotModuleReplacementPlugin()],
+    devtool: 'eval-cheap-module-source-map',
     devServer: {
         contentBase: resolve(__dirname, "./dist"),
         watchOptions: {
@@ -15,8 +17,10 @@ const devConfig = merge(commonConfig, {
         host: '0.0.0.0',
         compress: true,
         hot: true,
+        hotOnly: false,
         port: 5000,
         open: true,
+        inline: true,
         historyApiFallback: true
     },
 })
